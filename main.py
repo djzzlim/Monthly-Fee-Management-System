bc619fb7dd048acb68620256b6e24b4991891d8c
Lim Kar Joon <djzzlim@gmail.com>
2025-02-12 00:21:32 +0800

    Modify Flask app for Render deployment

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from app.utilities.email import send_email
 app = create_app()
 
 if __name__ == "__main__":
-    # Run the Flask app
-    app.run(debug=True, use_reloader=False)  # use_reloader=False to avoid double execution
+    # Run Flask in production mode
+    app.run(host="0.0.0.0", port=10000, debug=False)
